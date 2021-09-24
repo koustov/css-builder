e86f9fae67f76bece20777ee6daa10535ee18bc1
root <root@INmaitrak01.microfocus.com>
2021-09-24 09:48:00 +0530

    added d

---
+++ example/build/precache-manifest.13533c946f79faf10a2f38f6b16fa47f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60f6b29fb7344d53c9df8450a3b32900",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f7a525aad9d3a5009b41",
+    "url": "./static/css/main.cb5a3c1a.chunk.css"
+  },
+  {
+    "revision": "077960b60001ca840a15",
+    "url": "./static/js/2.8b25ba20.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.8b25ba20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7a525aad9d3a5009b41",
+    "url": "./static/js/main.336ff386.chunk.js"
+  },
+  {
+    "revision": "8c9dcfb3db278dd8d82a",
+    "url": "./static/js/runtime-main.2224aa16.js"
+  }
+]);
