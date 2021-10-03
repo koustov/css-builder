7987a7937d8841c778fc122fba3f8147c564ed82
Tapas Adhikary <tapas.adhikary@gmail.com>
2021-10-03 12:34:26 +0530

    ✨ Added for hanging-puncuation

--- src/definitions/index.js
+++ src/definitions/index.js
@@ -4,8 +4,9 @@ import d from './d.json';
 import j from './j.json';
 import w from './w.json';
 import z from './z.json';
+import h from './h.json';
 import t2 from './definitions.json';
 
 import a from './a.json'
 
-export const Templates = a.concat(b, c, d, j, w, z);
+export const Templates = a.concat(b, c, d, h, j, w, z);
