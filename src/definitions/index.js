c5077b51ab21cce7ae8b4c2bf75042556d2341c2
Maku <eemike07@gmail.com>
2021-10-03 22:17:22 -0400

    Adds 'V' attributes

--- src/definitions/index.js
+++ src/definitions/index.js
@@ -2,10 +2,11 @@ import b from './b.json';
 import c from './c.json';
 import d from './d.json';
 import j from './j.json';
+import v from './v.json';
 import w from './w.json';
 import z from './z.json';
 import t2 from './definitions.json';
 
 import a from './a.json'
 
-export const Templates = a.concat(b, c, d, j, w, z);
+export const Templates = a.concat(b, c, d, j, v, w, z);
